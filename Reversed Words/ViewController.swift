3952af15536248607e729b50f28ffc234f310d37
DmitriyFod <dzyaruk2002@gmail.com>
2022-04-09 16:50:00 +0300

    Reversed

---
+++ Reversed Words/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Reversed Words
+//
+//  Created by Admin on 04.04.2022.
+//
+
+import UIKit
+import Foundation
+
+class ViewController: UIViewController {
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    
+    }
+    @IBOutlet weak var InputText: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var reverseButton: UIButton!
+    @IBAction func ReverseButtonPressed(_ sender: Any) {
+        resultLabel.text = ReversedWords(sentence: (InputText.text!))
+    }
+    
+    func ReversedWords (sentence: String) ->String{
+        var newSentence = ""
+        let allWords = InputText.text!.components(separatedBy: " ")
+        for word in allWords{
+            if newSentence != ""{
+                newSentence += " "
+            }
+            let reverseWord = String(word.reversed())
+            newSentence += reverseWord
+        }
+                
+        return newSentence
+    }
+}
+
